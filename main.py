fc53824a65b67eb73ff1d2856867df80790dff8d
Giray Hakan <girayhakan23@gmail.com>
2023-12-03 14:07:30 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import os
+from dotenv import load_dotenv
+
+from langchain.prompts import PromptTemplate
+from langchain.agents import initialize_agent, Tool
+from langchain.agents import AgentType
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import MessagesPlaceholder
+from langchain.memory import ConversationSummaryBufferMemory
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.chains.summarize import load_summarize_chain
+from langchain.tools import BaseTool
+from pydantic import BaseModel, Field
+from typing import Type
+from bs4 import BeautifulSoup
+import requests
+import json
+from langchain.schema import SystemMessage
+
+load_dotenv()
+brwoserless_api_key = os.getenv("BROWSERLESS_API_KEY")
+serper_api_key = os.getenv("SERP_API_KEY")
